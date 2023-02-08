222735c0889a8d1349e8dc3148a474bdd50601c7
Julius <baladad.julius@gmail.com>
2023-02-08 13:08:33 -0800

    Add exercise to practice printing

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print ("Hello World!")# intro text
+print ("Hello Again!")# This is all mostly to get the reader comfortable with typing in a text editor and to introduce the print() function
+print ("I like typing this")
+print("This is fun")
+print('Yay! Printing')
+print("I'd much rather you 'not'")
+print('I "said" do not touch this')
