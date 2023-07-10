0a12878184060a4ef1aea056904b64bdaa8ca575
Czedrick <baladad.czedrick@gmail.com>
2023-07-10 13:26:39 -0700

    Add cm and kg conversion

--- ex5.py
+++ ex5.py
@@ -1,18 +1,22 @@
 #more variables and even more printing
-my_name = 'Zed A. Shaw'
-my_age = 35
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'blue' 
-my_teeth = 'white'
-my_hair = 'brown'
+name = 'Zed A. Shaw'
+age = 35
+height = 74 # inches
+height_cm = height*2.54 
+weight = 180 # lbs
+weight_kg = weight * .453592
+eyes = 'blue' 
+teeth = 'white'
+hair = 'brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {height_cm} cm tall.")
+print(f"He's {weight} pounds heavy")
+print(f"He's also {weight_kg} kg!")
 print("Actually that's not heavy")
-print(f'''He's  got {my_eyes} and {my_hair} hair.
-His teeth are usually {my_teeth} depending on the coffee''')
+print(f'''He's  got {eyes} and {hair} hair.
+His teeth are usually {teeth} depending on the coffee''')
 
-total = my_age + my_height + my_weight 
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight 
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
