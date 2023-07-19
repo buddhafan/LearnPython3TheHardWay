234b0ed2dd100b5cae4bcd4a222e9d2d195e4e91
Czedrick <baladad.czedrick@gmail.com>
2023-07-19 14:31:01 -0700

    Add exercise about escape sequences and input

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end = ' ')
+age = input()
+
+print("How tall are you?(in)", end = ' ')
+height = input()
+print("How much do you weigh(lbs)?" , end = ' ')
+weight = input()
+
+print(f"So you're {age} years old, {height} tall and {weight} heavy.")
