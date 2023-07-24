962afaedf618133575d43057db6f1a88b2c59e76
Czedrick <baladad.czedrick@gmail.com>
2023-07-24 14:27:18 -0700

    Add exercise introducing modules

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+#read the WYSS secion for how to run this
+script, first, second, third = argv 
+
+print("The script is called:", script)
+print("The first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+opinion = input("What do you think? ")
